45f6e9a3ba8308fa723a9c0d4bf102bd74cf6b23
eric pan <kehanpan2018@u.northwestern.edu>
2018-02-27 10:46:54 -0600

    add new should work again

--- application.py
+++ application.py
@@ -3,6 +3,8 @@ import os
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 import pandas as pd
+from flask_sqlalchemy import SQLAlchemy
+import psycopg2
 
 
 
@@ -11,24 +13,63 @@ application.config.from_object(__name__) # load config from this file , flaskr.p
 
 # Load default config and override config from an environment variable
 
-'''
-driver = 'postgresql+psycopg2://'
 
-application.config['SQLALCHEMY_DATABASE_URI'] = driver \
-                                        + os.environ['RDS_USERNAME'] + ':' + os.environ['RDS_PASSWORD'] \
-                                        +'@' + os.environ['RDS_HOSTNAME']  +  ':' + os.environ['RDS_PORT'] \
-                                        + '/' + os.environ['RDS_DB_NAME']
-'''
+POSTGRES_USER = 'eric'
 
-application.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://root:19950519@ericdbinstance.cidcmcwt0iep.us-west-2.rds.amazonaws.com:5432/movies'
-#DB_URL = 'postgresql+psycopg2://root:19950519@aaf7l9wrkrsj4t.cidcmcwt0iep.us-west-2.rds.amazonaws.com:5432/'+os.environ['RDS_DB_NAME']
+POSTGRES_PW = '950519'
+
+POSTGRES_URL = "127.0.0.1:5432"
+
+POSTGRES_DB = 'movies'
+
+
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
 DB_URL = 'postgresql+psycopg2://root:19950519@ericdbinstance.cidcmcwt0iep.us-west-2.rds.amazonaws.com:5432/movies'
 
-data = pd.read_csv("result.csv")
-#data.to_csv("result.csv")
-#print(data)
+application.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation warning
+
+
+def connect_db():
+    """Connects to the specific database."""
+    return SQLAlchemy(application) 
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'postgres_db'):
+        g.postgres_db = connect_db()
+    return g.postgres_db
+
+@application.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'postgres_b'):
+        g.postgres_db.close()
+
+def init_db():
+    '''initialize the database'''
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+def show_entries():
+    '''get the result data from database'''
+    db = get_db()
+    cur = db.execute('select * from entries order by id desc')
+    return cur.fetchall
+
+def add_entry():
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+
 def out_similar(movieid):
-    #print(movieid)
+    '''get similar movie and related infos of the searched object; return none for exception'''
     try:
         result = data2.loc[data2['0'] == movieid]
         for i in result.iterrows():
@@ -42,29 +83,9 @@ def out_similar(movieid):
     except:
         return 0
 
-'''
-@application.route('/')
-def main():
-    with open('movielist','r') as f:
-        movies = f.read().split('\n')
-    return render_template('home.html',movieList = movies)
-
-
-@application.route('/movie', methods=['GET', 'POST'])
-def searchMovie():
-    error = None
-    if request.method == 'POST':
-        movieid = request.form['search']
-        result = out_similar(movieid)
-        print(result)
-        if result == None:
-            return render_template('404.html')
-        return render_template('mainpage.html', movieList=result)
-    return render_template('mainpage.html', movieList=out_similar("The Dark Knight Rises"))
-
-'''
 @application.route('/', methods=['GET', 'POST'])
 def searchMovie():
+    '''searchMovie main page function'''
     error = None
     if request.method == 'POST':
         movieid = request.form['search']
@@ -77,20 +98,23 @@ def searchMovie():
 
 @application.route('/contact')
 def contact():
+    '''contact page function'''
     return render_template('contact.html')
 
 @application.route('/resume')
 def resume():
+    '''resume page function'''
     return render_template('resume.html')
 
 if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
     data = pd.read_csv("result.csv")
     from sqlalchemy import create_engine
     engine = create_engine(DB_URL)
-    
     #data.to_sql(name='result', con = engine, if_exists = 'append', index=False)
     data2 = pd.read_sql_query('select * from result',con=engine)
+    # Setting debug to True enables debug output. This line should be
+    print(data2.loc[data2['0'] == 'Batman'])
+   # print(pd.read_sql_query("select * from client_history where '0' ='Batman'",con=engine))
+    # removed before deploying a production app.
     application.debug = True
     application.run()
